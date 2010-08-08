91fffd81ba83e0cf93dc282f9d4353f7badd0572
Stanisław Kolarzowski <stanislaw.kolarzowski@gmail.com>
2010-08-08 19:49:36 +0200

    Fixed typos in comments

--- lib/easycrumbs/collection.rb
+++ lib/easycrumbs/collection.rb
@@ -1,19 +1,19 @@
 module EasyCrumbs
-  class Collection    
+  class Collection
     attr_reader :breadcrumbs, :route, :path
-    
+
     def initialize(request, options = {})
       @request = request
-      
+
       @route = find_route
       @path = find_path
       @controller = @path[:controller]
       @action = @path[:action]
-      
+
       @pathes = make_pathes
       @breadcrumbs = make_breadcrumbs(options)
     end
-    
+
     # Finding route with given path and method
     # Return ActionController:Routing::Route object
     def find_route
@@ -23,15 +23,15 @@ module EasyCrumbs
       raise EasyCrumbs::NotRecognized if routes.empty?
       routes.first
     end
-    
+
     # Return hash with path parameter
     # for example:
     # { :controller => 'movies', :action => 'show', :country_id => '23', :id => '12' }
     def find_path
       @route.recognize(request_path, :method => request_method)
     end
-    
-    # Select only static and dynamic segments form route. Static segments points at controllers and dynamic points at models. 
+
+    # Select only static and dynamic segments from route. Static segments points at controllers and dynamic points at models.
     # It is given in right order
     # If last segment is equal to member action then it should be deleted. for example movies/123/edit should not return "edit" segment
     def segments
@@ -41,13 +41,13 @@ module EasyCrumbs
       segments.pop if segments.last.is_a?(ActionController::Routing::StaticSegment) && segments.last.value == @action
       segments
     end
-    
+
     # Returning controller object from static segment
     def pick_controller(segment)
       "#{segment.value.titlecase}Controller".constantize.new
     end
-    
-    # Retrung model object form dynamic segment
+
+    # Retrung model object from dynamic segment
     # If key has not model name then it is taken from current controller(it is taken from path)
     def pick_model(segment)
       key = segment.key
@@ -59,7 +59,7 @@ module EasyCrumbs
       model = model.titlecase.constantize
       model.find(@path[key])
     end
-    
+
     # Retruning array of controllers and models objects from right segments
     # for example
     # [#<CountriesController:0x001>, #<Country:0x001 @name="usa">, #<MoviesController:0x001>, #<Movie:0x001 @name="titanic">]
@@ -72,7 +72,7 @@ module EasyCrumbs
         end
       end
     end
-    
+
     # Return array of breadcrumbs object in right order
     def make_breadcrumbs(options = {})
       breadcrumbs = [Breadcrumb.new(ApplicationController.new)]
@@ -83,7 +83,7 @@ module EasyCrumbs
       end
       breadcrumbs
     end
-    
+
     # Retrurn parameters for path of model
     # If it is last object then action is equal to request action
     def path_for_model(segment)
@@ -94,12 +94,12 @@ module EasyCrumbs
         {:action => 'show', key => @path[key]}
       end
     end
-    
+
     # Retrun parameters for path of controller
     def path_for_controller(segment)
       {:action => 'index', :controller => segment.value}
     end
-    
+
     # If controller name is connected with object then parameter should be :id instead of :object_id
     # {:controller => 'movies', :movie_id => 1} will be {:controller => 'movies', :id => 1}
     def repaired_model_path(path)
@@ -108,11 +108,11 @@ module EasyCrumbs
       id = path.delete(object_param)
       id.nil? ? path : path.merge({:id => id})
     end
-    
+
     # Retrun array of pathes for every segment
     # for example:
     # countries > 1 > movies > 2 > actors> 3
-    # 
+    #
     # {:action => 'index', :controller => 'countries'},
     # {:action => 'show', :controller => 'countries', :id => 1},
     # {:action => 'index', :controller => 'movies', :country_id => 1},
@@ -131,11 +131,11 @@ module EasyCrumbs
         result.dup
       end
     end
-    
+
     def render(options = {})
       options[:separator] ||= " > "
       options[:last_link] = true if options[:last_link].nil?
-      
+
       elements = @breadcrumbs.map do |breadcrumb|
         if options[:last_link] == false && breadcrumb == @breadcrumbs.last
           breadcrumb.name
@@ -146,15 +146,15 @@ module EasyCrumbs
       elements.join(options[:separator])
     end
     private
-    
+
     def request_path
       @request.path
     end
-    
+
     def request_method
       @request.method
     end
-    
+
     def link_to(name, path)
       "<a href=\"#{path}\">#{name}</a>"
     end
